0a896c3f9a1bec0c11114bd812dc1d87d5ee3e82
ndoniyor <dnimatullo@gmail.com>
2023-03-06 16:58:48 -0500

    sync local and remote repo

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
-# flashcards
-flashcard web app made with React
+# Web Development Project 1 - *Operating Systems Flashcards*
+
+Submitted by: **Doniyor Nimatullo**
+
+This web app: **Flashcards web app that randomly outputs flashcards pertaining to various topics in operating systems.**
+
+Time spent: **1.5** hours spent in total
+
+## Required Features
+
+The following **required** functionality is completed:
+
+- [x] **The title of the card set and some information about it, such as a short description and the total number of cards are displayed**
+- [x] **A single card at a time is displayed, only showing one of the components of the information pair**
+- [x] **A list of card pairs is created**
+- [x] **Clicking on the card shows the corresponding component of the information pair**
+- [x] **Clicking the next button displays a random new card**
+
+The following **optional** features are implemented:
+
+- [ ] Cards contains images in addition to or in place of text
+- [ ] Cards have different visual styles such as color based on their category
+- [x] *visual style implemented*
+
+The following **additional** features are implemented:
+
+* [ ] List anything else that you added to improve the site's functionality!
+
+## Video Walkthrough
+
+Here's a walkthrough of implemented required features:
+
+<img src='https://media1.giphy.com/media/v1.Y2lkPTc5MGI3NjExYzQwYmRjMjJhOTViMGQzZGYyNDc3NGY2YjcyOGM5NmY0MTJjZDZjZCZjdD1n/uuSJnv2XuuKsKgpyDV/giphy.gif' title='Video Walkthrough' width='' alt='Video Walkthrough' />
+
+<!-- Replace this with whatever GIF tool you used! -->
+GIF created with Kooha (Linux)
+
+## Notes
+
+The biggest challenge was implementing the random feature for the next card button, and being able to go back to the previous card because of that.
+
+## License
+
+    Copyright [2024] [Doniyor Nimatullo]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,101 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const questionsAnswers = {
+  1: {
+    question: "What are the two types of I/O devices?",
+    answer: "Block and character devices",
+  },
+  2: {
+    question: "How does memory-mapped I/O help with reading/writing registers with C?",
+    answer: "I/O with MMI/O can be addressed as a variable in C since it is just in memory.",
+  },
+  3: {
+    question: "Why does MMI/O not need special protection mechanisms?",
+    answer:"The OS can just not put the portion of memory containing the control registers in any user’s virtual address space.",
+  },
+  4: {
+    question: "How does MMI/O have compatibility between memory and control register instructions?",
+    answer: "Every instruction that accesses memory can also access I/O",
+  },
+  5: {
+    question: "What two elements are needed to implement R-Format ALU operations?",
+    answer: "Register file and ALU",
+  },
+  6: {
+    question: "What happens when a process needs to grow in the heap but is next to a process that is blocking the space it needs?",
+    answer: "The first process must be suspended until space is freed; or it is killed",
+  },
+  7: {
+    question: "How does room for growth of a process work in a data segment with a heap and a stack?",
+    answer: "The process’ heap grows upwards and the stack grows downwards and the area in-between is the room for growth.",
+  },
+  8: {
+    question: "Which physical component maps virtual addresses to physical addresses?",
+    answer: "Memory Management Unit (MMU)",
+  },
+  9: {
+    question: "What are the units in virtual memory and their corresponding unit in physical memory called?",
+    answer:"Pages and page frames.",
+  },
+  10: {
+    question:"What bit keeps track of which pages are physically present in memory?",
+    answer: "Present/absent bit",
+  }
+}
+
+const deckLength = Object.keys(questionsAnswers).length;
+
+const App = () => {
+  const [prevNumber, setPrev] = useState(1);
+
+  const [questionNumber, setNumber] = useState(1);
+
+  const [isFront, setFront] = useState(true);
+  
+  const handleClick = () =>{
+    setFront(!isFront);
+  }
+  
+  const nextQuestion = () => {
+    let randIndex;
+    do{
+      randIndex = Math.floor(Math.random() * deckLength) + 1;
+    }while (randIndex === questionNumber);
+
+    setPrev(questionNumber)
+    setNumber(randIndex);
+    setFront(true);
+  }
+
+  const prevQuestion = () =>{
+      setNumber(prevNumber)
+  }
 
   return (
     <div className="App">
-      <h1>Operating Systems: Memory</h1>
-      <div className="flashcard-container">
-        a
+      <h1>Operating Systems Review</h1>
+      <h2>Number of cards: {deckLength}</h2>
+      <h3>Test your knowledge on memory, I/O and instruction processing!</h3>
+
+      <div className="flashcardContainer" onClick={handleClick}>
+        {isFront ? (
+          <div className='flashcardQuestion'>
+            {questionsAnswers[questionNumber].question}
+          </div>
+        ) : (
+          <div className='flashcardAnswer'>
+            {questionsAnswers[questionNumber].answer}
+           </div>
+        )}
+      </div>
+      <div className="btnContainer">
+        <div className="previousBtn">
+          <button className="navBtn" onClick={prevQuestion}>←</button>
+        </div>
+        <div className="nextBtn">
+          <button className="navBtn" onClick={nextQuestion}>→</button>
+        </div>
       </div>
     </div>
   )
