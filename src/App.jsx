00db13abfabe2bd84cc639ddf8e2c4e4569ef68b
ndoniyor <dnimatullo@gmail.com>
2023-03-06 15:13:18 -0500

    sync local and remote repo

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Operating Systems: Memory</h1>
+      <div className="flashcard-container">
+        a
+      </div>
+    </div>
+  )
+}
+
+export default App
